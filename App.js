8e1d48861921f4669e29a5039b2ee653b28566dc
Heesung jang <heesungj7@gmail.com>
2021-10-02 01:58:50 +0900

    [feat] basic css work

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
     return (
-        <View style={{ flex: 1 }}>
-            <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-            <View style={{ flex: 3, backgroundColor: "teal" }}></View>
-            <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+        <View style={styles.container}>
+            <View style={styles.city}>
+                <Text style={styles.cityName}>Seoul</Text>
+            </View>
+            <View style={styles.weather}>
+                <View style={styles.day}>
+                    <Text style={styles.temperature}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+            </View>
+            <StatusBar style="light" />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "tomato",
+    },
+    city: {
+        flex: 1.2,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    cityName: {
+        color: "black",
+        fontSize: 68,
+        fontWeight: "500",
+    },
+    weather: {
+        flex: 3,
+    },
+    day: {
+        flex: 1,
+        alignItems: "center",
+    },
+    temperature: {
+        marginTop: 50,
+        fontSize: 178,
+    },
+    description: {
+        marginTop: -30,
+        fontSize: 60,
+    },
+});
