12f895040ed4507825d68c4aa39d21f9d2fae8db
Heesung jang <heesungj7@gmail.com>
2021-10-02 16:24:36 +0900

    [feat] expo.location 활용
    - get permission for location info from user
    
    - get current position: latitude and longitude
    
    - get actual location info with current position by using reverseGeocodeAsync

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
+import React, { useState, useEffect } from "react";
+
+import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
 import { StyleSheet, Text, Dimensions, View, ScrollView } from "react-native";
 
+// 모바일 스크린 사이즈
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [isPermitted, setIsPermitted] = useState(true);
+
+    const getPermission = async () => {
+        const permission = await Location.requestForegroundPermissionsAsync();
+        setIsPermitted(permission.granted);
+        const {
+            coords: { latitude, longitude },
+        } = await Location.getCurrentPositionAsync({
+            accuracy: 5,
+        });
+        const location = await Location.reverseGeocodeAsync({
+            latitude,
+            longitude,
+        });
+        console.log(location);
+    };
+
+    useEffect(() => {
+        getPermission();
+    }, []);
     return (
         <View style={styles.container}>
             <View style={styles.city}>
