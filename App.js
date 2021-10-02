483a3c580ece07c322989a09d019018acf060f5a
Heesung jang <heesungj7@gmail.com>
2021-10-02 17:08:18 +0900

    get weather data and render

--- App.js
+++ App.js
@@ -2,38 +2,52 @@ import React, { useState, useEffect } from "react";
 
 import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, Dimensions, View, ScrollView } from "react-native";
+import {
+    StyleSheet,
+    Text,
+    Dimensions,
+    View,
+    ScrollView,
+    ActivityIndicator,
+} from "react-native";
 
 // 모바일 스크린 사이즈
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
+// openWeather api key
+const API_KEY = "535d8528839d562b4179a400168ca57b";
 
 export default function App() {
-    const [location, setLocation] = useState(null);
-    const [errorMsg, setErrorMsg] = useState(null);
+    const [city, setCity] = useState("Loading...");
+    const [days, setDays] = useState([]);
     const [isPermitted, setIsPermitted] = useState(true);
 
-    const getPermission = async () => {
+    // 날짜 요청 함수
+    const getWeather = async () => {
         const permission = await Location.requestForegroundPermissionsAsync();
         setIsPermitted(permission.granted);
         const {
             coords: { latitude, longitude },
-        } = await Location.getCurrentPositionAsync({
-            accuracy: 5,
-        });
-        const location = await Location.reverseGeocodeAsync({
-            latitude,
-            longitude,
-        });
-        console.log(location);
+        } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+        const location = await Location.reverseGeocodeAsync(
+            { latitude, longitude },
+            { useGoogleMaps: false }
+        );
+        setCity(location[0]?.city);
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+        );
+        const { daily } = await response.json();
+        setDays(daily);
     };
 
     useEffect(() => {
-        getPermission();
+        getWeather();
     }, []);
     return (
         <View style={styles.container}>
             <View style={styles.city}>
-                <Text style={styles.cityName}>Seoul</Text>
+                <Text style={styles.cityName}>{city}</Text>
             </View>
             <ScrollView
                 pagingEnabled
@@ -41,18 +55,26 @@ export default function App() {
                 showsHorizontalScrollIndicator={false}
                 contentContainerStyle={styles.weather}
             >
-                <View style={styles.day}>
-                    <Text style={styles.temperature}>27</Text>
-                    <Text style={styles.description}>Sunny</Text>
-                </View>
-                <View style={styles.day}>
-                    <Text style={styles.temperature}>27</Text>
-                    <Text style={styles.description}>Sunny</Text>
-                </View>
-                <View style={styles.day}>
-                    <Text style={styles.temperature}>27</Text>
-                    <Text style={styles.description}>Sunny</Text>
-                </View>
+                {days.length === 0 ? (
+                    <View style={styles.day}>
+                        <ActivityIndicator
+                            color="white"
+                            size="large"
+                            style={{ marginTop: 10 }}
+                        />
+                    </View>
+                ) : (
+                    days.map((day, idx) => (
+                        <View key={idx} style={styles.day}>
+                            <Text style={styles.temperature}>
+                                {parseFloat(day.temp.day).toFixed(1)}
+                            </Text>
+                            <Text style={styles.description}>
+                                {day.weather[0].main}
+                            </Text>
+                        </View>
+                    ))
+                )}
             </ScrollView>
             <StatusBar style="light" />
         </View>
